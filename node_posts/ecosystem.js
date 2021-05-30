3015d53e10fcefb911821d338830d6653ed601cd
Rico Chen <midnightcodr@gmail.com>
2021-05-29 21:52:50 -0400

    removed useless ecosystem.js

--- node_posts/ecosystem.js
+++
@@ -1,8 +0,0 @@
-    {
-    "apps" : [{
-      "name"      : "api",
-      "script"    : "main.js",// name of the startup file
-      "instances" : 4,          // number of workers you want to run
-      "exec_mode" : "cluster",  // to turn on cluster mode; defaults to 'fork' mode 
-    }]
-    }
